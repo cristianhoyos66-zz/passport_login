9466a3595f96466903943aeabf86fe245ac3f630
cristianhoyos66 <cristianhoyos66@hotmail.com>
2020-09-16 20:52:02 -0500

    Add gmail auth
    This authentication will work with gmail and local auth, but it will
    also work with sessions instead of JWT

--- passport-config.js
+++ passport-config.js
@@ -1,15 +1,17 @@
 const localStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const googleStrategy = require('passport-google-oauth2').Strategy;
+const argon2 = require('argon2');
 
 function initialize(passport, getUserByEmail, getUserById) {
+    let user = {};
     const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email);
+        user = getUserByEmail(email);
         if (user == null) {
             return done(null, false, { message: 'No user with that email' });
         }
 
         try {
-            if (await bcrypt.compare(password, user.password)) {
+            if (await argon2.verify(user.password, password)) {
                 return done(null, user);
             } else {
                 return done(null, false, { message: 'Password Incorrect' });
@@ -19,11 +21,35 @@ function initialize(passport, getUserByEmail, getUserById) {
         }
     };
 
+    const gmailAuthenticate = function(req, accessToken, refreshToken, profile, done) {
+        user = {
+            id: Date.now().toString(),
+            name: profile.displayName,
+            email: profile.email,
+            password: ''
+        };
+        return done(null, user);
+    };
+
     passport.use(new localStrategy({ usernameField: 'email' }, authenticateUser));
-    passport.serializeUser((user, done) => done(null, user.id));
+    passport.use(
+        new googleStrategy(
+            {
+                clientID: process.env.GMAIL_CLIENT_ID,
+                clientSecret: process.env.GMAIL_CLIENT_SECRET,
+                callbackURL: '/auth/google/callback',
+                passReqToCallback: true
+            },
+            gmailAuthenticate
+        )
+    );
+
+    passport.serializeUser((user, done) => {
+        return done(null, user.id);
+    });
 
     passport.deserializeUser((id, done) => {
-        return done(null, getUserById(id));
+        return done(null, user);
     });
 }
 
--- server.js
+++ server.js
@@ -11,18 +11,21 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 
 const initializePassport = require('./passport-config');
+const argon2 = require('argon2');
 initializePassport(passport, email => users.find(user => user.email === email), id => users.find(user => user.id === id));
 
 const users = [];
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use('/js', express.static('js'));
 app.use(flash());
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
         resave: false,
-        saveUnitialized: false
+        saveUnitialized: false,
+        cookie: { maxAge:60000 }
     })
 );
 app.use(passport.initialize());
@@ -47,13 +50,43 @@ app.post(
     })
 );
 
+app.get(
+    '/gmail-login',
+    passport.authenticate('google', {
+        scope: ['profile', 'email', 'https://www.googleapis.com/auth/userinfo.email']
+    })
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+);
+
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs');
 });
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
+/*app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+});*/
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await argon2.hash(req.body.password, { type: argon2.argon2id });
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
